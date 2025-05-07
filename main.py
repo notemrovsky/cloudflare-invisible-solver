38fea718384d54d73852afa15d9f7493e1da11bb
BoarIncorporated <boarincorporated@protonmail.com>
2025-05-07 14:52:40 +0200

    No deobfuscating

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import random
-import subprocess
-import tempfile
 from typing import List, Optional, Union
 
 import structlog
@@ -27,70 +25,50 @@ class CfSolver:
 
     def get_solution(self, challenge_url: str) -> Union[bool, str]:
         session = self._create_session()
-
         challenge_website = challenge_url.split("/cdn-cgi/")[0]
+        
         cloudflare_result = session.get(challenge_url)
         cf_ray_id = cloudflare_result.headers["cf-ray"]
 
         cf_ray_id = cf_ray_id.split("-")[0]
 
-        cloudflare_result = cloudflare_result.content
 
-        with tempfile.NamedTemporaryFile(
-            suffix=".js", delete=False
-        ) as temp_js_file, tempfile.NamedTemporaryFile(
-            suffix=".js", delete=False
-        ) as temp_js_2_file:
-            temp_js = temp_js_file.name
-            temp_js_2 = temp_js_2_file.name
+        cfs = cloudflare_result.text
 
-            with open(temp_js, "wb") as f:
-                f.write(cloudflare_result)
+        jsd = cfs.split("/jsd/r/")[1].split(',')[0]
 
-            subprocess.run(
-                f"obfuscator-io-deobfuscator {temp_js} -o {temp_js_2}",
-                shell=True,
-                capture_output=True,
-                text=True,
-                check=False
-            )
+        cloudflare_enc_key = constants.ENC_KEY_RE.search(cfs).group(0)
 
-            with open(temp_js_2, "r", encoding="utf-8") as f:
-                deobfuscated_result = f.read()
+        challenge_url = (
+            f"{challenge_website}/cdn-cgi/challenge-platform/h/b/jsd/r/"
+            + jsd
+            + cf_ray_id
+        )
 
-            cloudflare_enc_key = deobfuscated_result.split('".charAt(')[0].split('"')[
-                -1
-            ]
-            challenge_url = (
-                f"{challenge_website}/cdn-cgi/challenge-platform/h/b/jsd/r/"
-                + deobfuscated_result.split("/jsd/r/")[1].split('"')[0]
-                + cf_ray_id
-            )
+        wb_result = create_wb_result(
+            session.headers["user-agent"],
+            cloudflare_enc_key,
+            challenge_website
+        )
 
-            wb_result = create_wb_result(
-                session.headers["user-agent"],
-                cloudflare_enc_key,
-                challenge_website
-            )
+        logger.info(
+            "Solving cloudflare challenge.",
+            challenge_url=challenge_url[:50] + "...",
+            encoded_payload=wb_result[:30] + "..",
+        )
 
+        session.headers = constants.CHALLENGE_HEADERS.copy()
+        session.headers["origin"] = challenge_website
+        response = session.post(challenge_url, data=wb_result)
+
+        if response.status_code == 200:
             logger.info(
-                "Solving cloudflare challenge.",
-                challenge_url=challenge_url[:50] + "...",
-                encoded_payload=wb_result[:30] + "..",
+                "Cloudflare challenge successfully solved.",
+                cf_clearance=session.cookies["cf_clearance"][:50] + "..",
             )
-
-            session.headers = constants.CHALLENGE_HEADERS.copy()
-            session.headers["origin"] = challenge_website
-            response = session.post(challenge_url, data=wb_result)
-
-            if response.status_code == 200:
-                logger.info(
-                    "Cloudflare challenge successfully solved.",
-                    cf_clearance=session.cookies["cf_clearance"][:50] + "..",
-                )
-                return session.cookies["cf_clearance"]
-            logger.error("Error happened while solving cloudflare.")
-            return False
+            return session.cookies["cf_clearance"]
+        logger.error("Error happened while solving cloudflare.")
+        return False
 
 
 if __name__ == "__main__":
--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,5 @@
+import re
+
 DEFAULT_HEADERS = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
     "accept-language": "en-US,en;q=0.8",
@@ -33,3 +35,5 @@ CHALLENGE_HEADERS = {
     "sec-gpc": "1",
     "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
 }
+
+ENC_KEY_RE = re.compile(r'[a-zA-Z0-9+\-$]{65}')
